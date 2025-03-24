d83b810eafc00ae5547c8f90dd7b5c0e728b9cd8
Mohamed Habib <moe.habib9@gmail.com>
2025-03-24 17:37:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-resource "null_resource" "test26" {}
+# Generate 10,000 null resources using count
+resource "null_resource" "example" {
+  count = 10000
+  
+  triggers = {
+    id = count.index
+  }
+}
 
 #variable "TEST" {
 #  default = "hello"
