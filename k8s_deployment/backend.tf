0ed7bd7e3ddac73e50ea3d96c014221b39962c5e
Brian Reardon <breardon2011@gmail.com>
2025-10-08 10:56:54 -0700

    add config

---
+++ k8s_deployment/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # Configuration will come from tf_backend.tfbackend
+    # When using workspaces, state files will be stored at:
+    # - env:/shared/terraform.tfstate
+    # - env:/dev/terraform.tfstate
+    # - env:/qa/terraform.tfstate
+    # - env:/prod/terraform.tfstate
+  }
+}
+
