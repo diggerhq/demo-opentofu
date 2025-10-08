95a2af7194db45d96dde305e402939fa9a692a51
Brian Reardon <breardon2011@gmail.com>
2025-10-08 11:58:52 -0700

    back to null resources

--- k8s_deployment/dev.tf
+++ k8s_deployment/dev.tf
@@ -1,17 +1,4 @@
-resource "aws_ssm_parameter" "dev_config" {
-  name        = "/k8s_deployment/dev/config"
-  description = "Configuration parameter for dev environment"
-  type        = "SecureString"
-  value       = jsonencode({
-    environment = "dev"
-    timestamp   = timestamp()
-  })
-
-  tags = {
-    Environment = "dev"
-    ManagedBy   = "terraform"
-  }
-}
+resource "null_resource" "dev" {}
 
 
 
--- k8s_deployment/prod.tf
+++ k8s_deployment/prod.tf
@@ -1,14 +1,1 @@
-resource "aws_ssm_parameter" "prod_config" {
-  name        = "/k8s_deployment/prod/config"
-  description = "Configuration parameter for prod environment"
-  type        = "SecureString"
-  value       = jsonencode({
-    environment = "prod"
-    timestamp   = timestamp()
-  })
-
-  tags = {
-    Environment = "prod"
-    ManagedBy   = "terraform"
-  }
-}
+resource "null_resource" "prod" {}
