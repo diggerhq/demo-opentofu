88c39f78b3f302b912218d5720c586b90122759d
Mohamed Habib <moe.habib9@gmail.com>
2025-11-05 13:11:31 -0800

    Update workspace name in main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -3,7 +3,7 @@ terraform {
     hostname = "mo-digger-test.ngrok.app"
     organization = "org_01K89A2ZCCJHY1Z393WQ08BVDM"    
     workspaces {
-      name = "17f98e72-3c16-4e08-9b01-ad78a1eb05ed"
+      name = "e20883a2-9a92-4769-b579-94a908ec3bbb"
     }
   }
 }
