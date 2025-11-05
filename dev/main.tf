327279dca57c0543fce1e69f5f719ebd9afecd60
Mohamed Habib <moe.habib9@gmail.com>
2025-11-05 07:20:33 -0800

    Add Terraform cloud configuration
    Add Terraform cloud configuration with workspace details.

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,12 @@
+terraform {
+  cloud {
+    hostname = "otaco.app"
+    organization = "org_01K98XKY30W9P5NQEENQ5B5YE6"    
+    workspaces {
+      name = "1862270c-cfbd-4f29-899b-4871497a89cf"
+    }
+  }
+}
+
 
 resource "null_resource" "test3" {}
-
-
