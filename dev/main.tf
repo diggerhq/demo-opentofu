18dafbd5b956c7227428fa068ccc959856111604
Mohamed Habib <moe.habib9@gmail.com>
2025-11-21 10:37:27 -0800

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  cloud {
-    hostname = "otaco.app"
-    organization = "org_01K98XKY30W9P5NQEENQ5B5YE6"    
-    workspaces {
-      name = "d3fefb68-ec0c-4f2d-a971-13986c2e38c8"
-    }
-  }
-}
+
 
 resource "null_resource" "test3" {}
