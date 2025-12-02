e0a6ffed9cfcd6271eaf1e49afb358cb18bf32fb
Brian Reardon <breardon2011@gmail.com>
2025-12-02 12:28:08 -0800

    new pr

--- main.tf
+++ main.tf
@@ -1,4 +1,62 @@
+terraform {
+  cloud {
+    hostname = "brian-digger.ngrok.dev"
+    organization = "org_01K9X3SEYQY6DZB9EX1HE4Z3W6"    
+    workspaces {
+      name = "12781278-689d-4097-b711-9c99771dad23"
+    }
+  }
+}
 resource "null_resource" "test29" {}
+
+resource "null_resource" "test36" {}
+
+resource "null_resource" "test99" {}
+resource "null_resource" "test98" {}
+resource "null_resource" "test97" {}
+
+resource "null_resource" "test96" {}
+
+resource "null_resource" "test94" {}
+
+resource "null_resource" "test95" {}
+
+resource "null_resource" "test30" {}
+
+
+resource "null_resource" "test31" {}
+
+resource "null_resource" "test32" {}
+
+resource "null_resource" "test33" {}
+
+resource "null_resource" "test34" {} 
+
+resource "null_resource" "test35" {} 
+
+
+resource "null_resource" "test37" {} 
+
+resource "null_resource" "test38" {} 
+
+resource "null_resource" "test39" {} 
+
+resource "null_resource" "test40" {} 
+
+resource "null_resource" "test41" {} 
+
+resource "null_resource" "test42" {} 
+
+resource "null_resource" "test43" {} 
+
+resource "null_resource" "test44" {} 
+
+
+resource "null_resource" "test_manual" {
+  triggers = {
+    timestamp = timestamp()
+  }
+}
 
 #variable "TEST" {
 #  default = "hello"
