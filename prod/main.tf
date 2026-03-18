0f0aa6ebbf5eb3535e2893b019358496a2f4022a
Mohamed Habib <moe.habib9@gmail.com>
2026-03-18 10:00:52 -0700

    refactor prod environment: replace test resource with api_gateway and monitoring

--- prod/main.tf
+++ prod/main.tf
@@ -1,1 +1,18 @@
-resource "null_resource" "test" {}
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+}
+
+resource "null_resource" "api_gateway" {}
+
+resource "null_resource" "monitoring_alerts" {
+  triggers = {
+    environment = "prod"
+    alert_email = "oncall@example.com"
+    pagerduty   = "true"
+  }
+}
