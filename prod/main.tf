c52b82a6b29d6553c11e596cd442015178cc1d7f
Mohamed Habib <moe.habib9@gmail.com>
2026-03-18 19:52:43 -0700

    add S3 backend for prod state

--- prod/main.tf
+++ prod/main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+
+  backend "s3" {
+    bucket = "digger-demo-opentofu-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "null_resource" "test" {}
