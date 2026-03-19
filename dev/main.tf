f0de8b08d1ac3b8d840ac99bf24a1c6e9d34a16c
motatoes <moe.habib9@gmail.com>
2026-03-18 20:34:45 -0700

    remove api_gateway and log_aggregator, simplify monitoring

--- dev/main.tf
+++ dev/main.tf
@@ -13,17 +13,10 @@ terraform {
   }
 }
 
-resource "null_resource" "api_gateway" {}
-
 resource "null_resource" "monitoring_alerts" {
   triggers = {
     environment = "dev"
     alert_email = "team@example.com"
-  }
-}
-
-resource "null_resource" "log_aggregator" {
-  triggers = {
-    log_level = "info"
+    slack_channel = "#dev-alerts"
   }
 }
